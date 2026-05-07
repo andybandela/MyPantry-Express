e80d464e925531cd0e87f2a6345e30fe69e8d5bb
andybandela <andybandela@gmail.com>
2026-05-07 01:52:08 -0400

    add category route, create getCategory controller.

--- controllers/category.js
+++ controllers/category.js
@@ -1,8 +1,26 @@
 const categories = require('../data');
+const Recipe = require('../models/recipe');
 
 
 exports.category = (req,res,next) =>{
     res.status(200).json({
         cat: categories.categories
     });
+}
+exports.getCategory = async(req,res) =>{
+    try {
+        const category = req.params.category;
+        console.log("category: ", category);
+        const recipes = await Recipe.find({ "metaInfo.category": category }).select('title description metaInfo.cuisineType metaInfo.tags');
+        if (!recipes) {
+            return res.status(404).json({ message: "No recipes found for this category" });
+        }
+        console.log("recipes: ",recipes);
+        return res.status(200).json({ recipes });
+    } catch (error) {
+        console.error("Error fetching recipes by category:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+    
+
 }
--- routes/category.js
+++ routes/category.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const categoryController = require('../controllers/category')
+const categoryController = require('../controllers/category');
 
 const router = express.Router();
 
-router.get('/',categoryController.category);
+//router.get('/',categoryController.category);
+router.get('/:category',categoryController.getCategory);
 
 
 module.exports = router;
